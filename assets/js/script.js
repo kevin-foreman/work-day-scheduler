64ef2da4a93f76f21a95002ca70a388dbaf6f458
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-09 16:58:08 -0600

    added a page timer to refresh every 5 minutes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ var currentTime = moment();
 
 currentDate.textContent = currentTime.format("MMMM DD, YYYY - hh:mm:ss a");
 
-// make the page refresh every 15 minutes so the user keeps track of tasks with the time of day
-setInterval(function() {
-    
-})
+// make the page refresh every 5 minutes so the user keeps track of tasks with the time of day
+var pageAuditTimer = function(everyFive) {
+    setTimeout("location.reload(true);",everyFive);
+};
+window.onload = pageAuditTimer(300000);
 
 // make tasks array
 var tasks = {
@@ -25,11 +26,16 @@ var tasks = {
 console.log(tasks);
 
 
+
+
 // Make the information put into form fields persist
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
+document.getElementById("saveBtn").addEventListener("click", function() {
+    var nineAm = document.getElementById("#nineAm").textContent;
+    localStorage.setItem("nineAm", JSON.stringify(tasks));
 
+})
+};
 
 // Get the information from local storage, and put it back on the page after refresh
 var loadTasks = function() {

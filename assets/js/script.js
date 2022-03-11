04746987b432eb408a386f58a8484dfe9bc3fa89
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-11 16:12:50 -0600

    finalized local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    // get current date and display it on screen
   var currentDate = document.querySelector("#current-day");
   var currentTime = moment();
   currentDate.textContent = currentTime.format("MMMM DD, YYYY - hh:mm:ss a");
@@ -8,36 +10,53 @@ $(document).ready(function() {
   var pageAuditTimer = function(everyFive) {
       setTimeout("location.reload(true);", everyFive);
   };
-
-
   window.onload = pageAuditTimer(300000);
+
+  // save button puts information in local storage
   $(".saveBtn").on("click", function() {
     var description=$(this).siblings(".description").val()
     var time=$(this).parent().attr("id")
     localStorage.setItem(time, description)
     console.log(description, time)
+    upDater()
   })
 
+  // get information from local storage and post to page after reload
   $("#9 .description").val(localStorage.getItem("9"))
-  
+  $("#10 .description").val(localStorage.getItem("10"))
+  $("#11 .description").val(localStorage.getItem("11"))
+  $("#12 .description").val(localStorage.getItem("12"))
+  $("#13 .description").val(localStorage.getItem("13"))
+  $("#14 .description").val(localStorage.getItem("14"))
+  $("#15 .description").val(localStorage.getItem("15"))
+  $("#16 .description").val(localStorage.getItem("16"))
+  $("#17 .description").val(localStorage.getItem("17"))
+
+// update the task fields with colors to correspond with the classes
 function upDater() {
   var currentHour = moment().hours
   $(".time-block").each(function() {
     var timeBlock = parseInt ($(this).attr("id"))
     if (timeBlock < currentHour) {
       $(this).addClass("past")
-    } else if
-  })
-}
+    } else if (timeBlock === currentHour) {
+      $(this).addClass("present")
+    } else if (timeBlock > currentHour) {
+      $(this).addClass("future")
+    }
+
+    }) 
+  }
 })
 
 
-// get current date and display it on screen
 
 
 
 
-// Make the information put into form fields persist
+
+
+// Old code
 
 
 // var nineAm = document.getElementById("nineAm");
